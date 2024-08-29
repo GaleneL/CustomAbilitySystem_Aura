d7c3f808d59ae974222d7b01be08a044722223dc
Eduardo Velez <galeneluna@gmail.com>
2024-08-29 15:11:58 -0600

    Added blueprint for Aura and Goblin_Spear Characters

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -7,6 +7,12 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
  	PrimaryActorTick.bCanEverTick = false;
 
+	// Setting up the weapon of all characters to the hand mesh and removing the
+	// collision of the socket/hand
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 }
 
 void AAuraCharacterBase::BeginPlay()
